766cf9d8662ee1cb18fcf190069e6fe8a925891c
Deeptha <deeptha.wickrema@hotmail.fr>
2018-05-08 15:45:17 +0200

    change gradle version

--- presentation/src/main/java/com/todo/presentation/view/DetailTaskActivity.java
+++ presentation/src/main/java/com/todo/presentation/view/DetailTaskActivity.java
@@ -15,14 +15,14 @@ import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
 public class DetailTaskActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
   @Inject
   DispatchingAndroidInjector<Fragment> fragmentDispatchingAndroidInjector;
   private static final String TAG = "DetailTaskActivity";
-  public static final String EXTRA_TASK_ID = "EXTRA_TASK_ID";
+  public static final  String EXTRA_TASK_ID = "EXTRA_TASK_ID";
 
   @Override
   protected void onSaveInstanceState(Bundle outState) {
-    Log.d(TAG, "onSaveInstanceState: set 5");
     outState.putInt("key", 5);
     super.onSaveInstanceState(outState);
   }
