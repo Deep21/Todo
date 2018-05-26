755465f0a7b82fd18486ea4bc3fedd08870fc8af
Deeptha <deeptha.wickrema@hotmail.fr>
2018-05-27 01:28:25 +0200

    refactor method

--- data/src/main/java/com/todo/data/datasource/ImplContactResolverApi.java
+++ data/src/main/java/com/todo/data/datasource/ImplContactResolverApi.java
@@ -41,77 +41,13 @@ public class ImplContactResolverApi implements IContactDataStore {
                             while (cursor.moveToNext()) {
 
                                 String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                                Cursor cursorPhone = context
-                                        .getContentResolver()
-                                        .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
-
-                                if (cursorPhone != null) {
-                                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                                    Log.d(TAG, "subscribe: " + name);
-                                    while (cursorPhone.moveToNext()) {
-                                        String phNumber = cursorPhone
-                                                .getString(cursorPhone
-                                                        .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                                        int phoneType = cursorPhone
-                                                .getInt(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
-
-                                        switch (phoneType) {
-                                            case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
-                                                Log.d(TAG, "subscribe: HOME " + phNumber);
-                                                break;
-                                            case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
-                                                Log.d(TAG, "subscribe: MOBILE " + phNumber);
-                                                break;
-                                            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
-                                                Log.d(TAG, "subscribe: WORK " + phNumber);
-                                                break;
-                                        }
-                                    }
-
-                                    cursorPhone.close();
-                                }
-
-                                Cursor postalCursor = context
-                                        .getContentResolver()
-                                        .query(ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI, null, ContactsContract.Data.CONTACT_ID + " = " + contactId, null, null);
-
-                                if (postalCursor != null) {
-                                    int postalType = postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.TYPE);
-
-                                    while (postalCursor.moveToNext()) {
-                                        String street = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
-                                        String city = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
-                                        String country = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
-                                        Log.d(TAG, "subscribe:" + street + postalType);
-
-                                        switch (postalType) {
-                                            case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_HOME:
-                                                Log.d(TAG, "subscribe: home" + street);
-                                                break;
-                                            case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_OTHER:
-                                                Log.d(TAG, "subscribe: other" + street);
-                                                break;
-                                            case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_WORK:
-                                                Log.d(TAG, "subscribe: work" + street);
-                                                break;
-                                        }
-
-                                    }
-                                }
-
-                                Cursor emails = context
-                                        .getContentResolver()
-                                        .query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId, null, null);
-
-                                if (emails != null) {
-                                    while (emails.moveToNext()) {
-                                        String email = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-                                        Log.d(TAG, "email: " + email);
-                                    }
-                                }
+                                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                                getImageById(contactId);
 
                             }
+
+                            cursor.close();
+
                         }
 
                         emitter.onComplete();
@@ -121,6 +57,115 @@ public class ImplContactResolverApi implements IContactDataStore {
 
                 });
 
+    }
+
+    public void getNoteById(String id) {
+        String noteWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+        String[] noteWhereParams = new String[]{id, ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE};
+        Cursor noteCur = context.getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, noteWhere, noteWhereParams, null);
+
+        if (noteCur != null && noteCur.moveToFirst()) {
+            String note = noteCur.getString(noteCur.getColumnIndex(ContactsContract.CommonDataKinds.Note.NOTE));
+            noteCur.close();
+        }
+
+    }
+
+    public void getPhoneNumber(String id){
+        Cursor cursorPhone = context
+                .getContentResolver()
+                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+
+        if (cursorPhone != null) {
+            while (cursorPhone.moveToNext()) {
+                String phNumber = cursorPhone
+                        .getString(cursorPhone
+                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                int phoneType = cursorPhone
+                        .getInt(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+
+                switch (phoneType) {
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                        Log.d(TAG, "subscribe: HOME " + phNumber);
+                        break;
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                        Log.d(TAG, "subscribe: MOBILE " + phNumber);
+                        break;
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                        Log.d(TAG, "subscribe: WORK " + phNumber);
+                        break;
+                }
+            }
+
+            cursorPhone.close();
+        }
+
+    }
+
+    public void getAddressById(String id){
+        Cursor postalCursor = context
+                .getContentResolver()
+                .query(ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI, null, ContactsContract.Data.CONTACT_ID + " = " + id, null, null);
+
+        if (postalCursor != null) {
+
+            while (postalCursor.moveToNext()) {
+                int postalType = postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.TYPE);
+
+                String street = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+                String city = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+                String country = postalCursor.getString(postalCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
+
+                switch (postalType) {
+                    case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_HOME:
+                        Log.d(TAG, "HOME address" + street);
+                        break;
+                    case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_OTHER:
+                        Log.d(TAG, "OTHER address" + street);
+                        break;
+                    case ContactsContract.CommonDataKinds.StructuredPostal.TYPE_WORK:
+                        Log.d(TAG, "WORK address"  + street);
+                        break;
+                }
+            }
+            postalCursor.close();
+
+        }
+    }
+
+
+    public void getImageById(String id){
+        String imWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+        String[] imWhereParams = new String[]{id, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE};
+        Cursor imCur = context.getContentResolver().query(ContactsContract.Data.CONTENT_URI,
+                null, imWhere, imWhereParams, null);
+
+        if (imCur != null && imCur.moveToFirst()) {
+            String imName = imCur.getString(imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.DATA));
+            String imType  = imCur.getString(imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.TYPE));
+            Log.d(TAG, "getImageById: " + imName);
+            Log.d(TAG, "getImageById: " + imType);
+            imCur.close();
+        }
+    }
+
+
+    public void getEmailbyId(int id){
+        Cursor emails = context
+                .getContentResolver()
+                .query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + id, null, null);
+
+        if (emails != null) {
+
+            while (emails.moveToNext()) {
+                int emailType = emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE);
+                String email = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                Log.d(TAG, "subscribe: home" + email);
+            }
+
+            emails.close();
+        }
     }
 
     @Override
